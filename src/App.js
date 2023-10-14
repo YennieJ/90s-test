4880022e76dc5084ef6265ab98f6758622121043
Jungmin Moon <jm.moon0801@gmail.com>
2023-10-14 15:28:00 +0900

    라우터 설정

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import "./App.css";
 import Login from "./Components/Login";
+import LoginId from "./Components/LoginId";
+import Number from "./Components/Number";
+import Qrcode from "./Components/Qrcode";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <BrowserRouter>
+        <Routes>
+        <Route path="/" element={<Navigate to="/loginid" replace />} />
+          <Route path="/" element={<Login />}>
+            <Route path="/loginid" element={<LoginId/>}/>
+            <Route path="/number" element={<Number/>}/>
+            <Route path="/qrcode" element={<Qrcode/>}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
