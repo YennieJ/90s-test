288465bd68d0961f79e14444459523cddae26ea5
Jungmin Moon <jm.moon0801@gmail.com>
2023-10-15 11:30:44 +0900

    로그인 기능 구현

--- src/Components/LoginId.jsx
+++ src/Components/LoginId.jsx
@@ -3,6 +3,8 @@ import React, { useState }  from "react";
 export default function LoginId() {
     const [email, setId] = useState("");
     const [password, setPassword] = useState("");
+    const [isSuccess, setIsSuccess] = useState("");
+    const [token, setToken] = useState("");
 
     // Input 입력값 가져오기
     const onHandleInput = (event) => {
@@ -11,16 +13,54 @@ export default function LoginId() {
     else if (name === "password") setPassword(value);
     };
 
+    // 로그인 함수
+    const login = async()=>{
+        const reqUrl = "/data/userData.json"
+        try{
+            const response = await fetch(reqUrl);
+            if(!response.ok){
+                throw new Error("데이터 불러오기에 실패했습니다.")
+            }
+            const data = await response.json();
+            const userData = data.user;
+
+            //userData에서 데이터 일치여부 확인
+            for(let i=0; i<userData.length; i++){
+                if(userData[i].email === email && userData[i].password === password){
+                    localStorage.setItem("token", userData[i].token);
+                    setToken(localStorage.getItem("token"));
+                    setIsSuccess(true);
+                    break;
+                } else { 
+                    setIsSuccess(false);
+                }
+            }
+        }catch(error){
+            console.error(error);
+        }
+    }
+
+    // submit 이벤트
+    const submitLogin = (event) =>{
+        event.preventDefault();
+        login(email, password);
+    }
+
     return (
-    <form>
-        <input type="email" name="email" value={email} onChange={onHandleInput} />
+    <form onSubmit={ submitLogin }>
+        <input type="email" name="email" value={ email } onChange={ onHandleInput } />
         <input
         type="password"
         name="password"
-        value={password}
-        onChange={onHandleInput}
+        value={ password }
+        onChange={ onHandleInput }
         />
         <button>로그인</button>
+        {
+            isSuccess === true ? (<p>로그인에 성공했습니다. <br/> 토큰 : { token }</p>) 
+            : isSuccess === false ? <p>아이디(로그인 전용 아이디) 또는 비밀번호를 잘못 입력했습니다. <br/> 입력하신 내용을 다시 확인해주세요.</p> 
+            : null
+        }
     </form>
     );
 }
